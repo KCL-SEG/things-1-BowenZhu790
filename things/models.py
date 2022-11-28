ed5b5ce60daf63fa307d0f7db4e376d15cf5157a
BOWEN ZHU <bowen.zhu@kcl.ac.uk>
2022-11-28 16:05:50 +0000

    add max length to charfields

--- things/models.py
+++ things/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 class Thing(models.Model):
     name = models.CharField(
-
+        max_length= 30,
     )
 
     description = models.CharField(
-        
+        max_length= 45,
     )
 
     quantity = models.IntegerField(
-        
+
     )
